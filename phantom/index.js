22ea71ed17d789c5a5d5d69f931a86bd8ca33c25
Dylan Fong <d2fong@uwaterloo.ca>
2017-05-11 11:39:38 -0400

    testing with 3.0, cose-bilkent

--- phantom/index.js
+++ phantom/index.js
@@ -1,6 +1,9 @@
 // prep the phantomjs page and expose globals that we can reference from node
 
 var cytoscape = window.cytoscape = require('cytoscape');
+var coseBilkent = require('cytoscape-cose-bilkent');
+
+coseBilkent( cytoscape );
 
 window.addEventListener('DOMContentLoaded', function(){
   var cy = window.cy = cytoscape({
